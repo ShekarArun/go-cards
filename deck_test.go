38025042d597e4ac690da1bcbd0e0b1f0fad1d25
Arun Shekar <shekar.arun@gmail.com>
2025-02-16 22:01:01 +0530

    test: Add tests for saveDeckToFile and newDeckFromFile functions

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -19,4 +22,23 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != lastCard {
 		t.Errorf("Expected last card to be %v, but got %v", lastCard, d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// Delete any existing test files
+	os.Remove("_decktesting")
+
+	// Seed test data
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	// Run test
+	deckLength := 52
+	if len(loadedDeck) != deckLength {
+		t.Errorf("Expected deck length of %v, but got %v", deckLength, len(loadedDeck))
+	}
+
+	// Cleanup any files created
+	os.Remove("_decktesting")
 }
